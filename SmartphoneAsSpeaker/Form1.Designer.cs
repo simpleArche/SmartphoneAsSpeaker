a70f7e41ba5f8e77ebf2ae90a34783c5b01359ae
simplearche <simplearche@gmail.com>
2017-12-03 18:19:42 +0200

    1) add function for geting local ip and function for checking if computer is in any local network 2)generate qr code on form load event

--- SmartphoneAsSpeaker/Form1.Designer.cs
+++ SmartphoneAsSpeaker/Form1.Designer.cs
@@ -28,21 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.picQRcode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picQRcode)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picQRcode
+            // 
+            this.picQRcode.BackColor = System.Drawing.SystemColors.Window;
+            this.picQRcode.Location = new System.Drawing.Point(12, 12);
+            this.picQRcode.Name = "picQRcode";
+            this.picQRcode.Size = new System.Drawing.Size(300, 300);
+            this.picQRcode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picQRcode.TabIndex = 0;
+            this.picQRcode.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(408, 304);
+            this.ClientSize = new System.Drawing.Size(327, 325);
+            this.Controls.Add(this.picQRcode);
             this.Name = "Form1";
             this.Text = "smartphoneAsSpeaker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picQRcode)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picQRcode;
     }
 }
 
--- SmartphoneAsSpeaker/Form1.cs
+++ SmartphoneAsSpeaker/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+
+using System.Net;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,34 @@ namespace SmartphoneAsSpeaker
         public Form1()
         {
             InitializeComponent();
+        }
+        public static string getLocalIp()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress addr in localIPs)
+            {
+                if (addr.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) return addr.ToString();
+            }
+            return "error";
+        }
+        public static bool isInNetwork()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach(IPAddress adress in localIPs){
+                if (adress.ToString() == "::1") return false;
+            }
+            return true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (isInNetwork())
+            {
+                string adress = getLocalIp();
+                Zen.Barcode.CodeQrBarcodeDraw qrcode = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+                picQRcode.Image = qrcode.Draw(adress, 20);
+            }
+            else MessageBox.Show("Computer isn't in any network");
         }
     }
 }
